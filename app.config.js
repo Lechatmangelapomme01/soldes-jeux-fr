a8e6a7504bea8897ac00c600e04ab6f24b9edd69
deborah <deborah.mishal@epitech.eu>
2026-02-03 06:50:33 +0100

    Enable static export for expo-router web

---
+++ app.config.js
@@ -0,0 +1,12 @@
+export default {
+  expo: {
+    name: "Soldes Jeux FR",
+    slug: "soldes-jeux-fr",
+    scheme: "soldesjeuxfr",
+    web: {
+      bundler: "metro",
+      output: "static"
+    },
+    plugins: ["expo-router"]
+  }
+};
